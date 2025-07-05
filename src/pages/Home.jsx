040c524daa39f7dbe8a795158241ec192af51e89
Aniket Kulkarni <avk84120@gmail.com>
2025-07-04 21:25:51 -0700

    Context API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Nav from '../Components/Nav'
 import Categories from '../../Category'
 import Card from '../Components/Card'
 import { food_items } from '../../food'
+import { dataContext } from '../Context/UserContext'
 
 const Home = () => {
+let {cate,setCate,input} = useContext(dataContext)
+
+function filter(category){
+    if(category==="All"){
+        setCate(food_items)
+    }else{
+        let newList= food_items.filter((item)=>(item.food_category===category))
+        setCate(newList)
+    }
+}
+
   return (
     <div className='bg-slate-200 w-full min-h-screen'>
       <Nav/>
-      <div className='flex flex-wrap justify-center gap-5 w-[100%] '>
+      {!input?<div className='flex flex-wrap justify-center gap-4 w-[100%] '>
         {Categories.map((item)=>{
             return <div className='w-[140px] h-[150px] bg-white flex flex-col items-start gap-5 p-5 
-            justify-start text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all duration-200 '>
+            justify-start text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all duration-200'
+            onClick={()=>filter(item.name)} >
                 {item.icon}
                 {item.name}
-             </div>
-             
-
-})}
-      </div>
+                </div>
+                })}
+             </div>:null}
+      
       <div className='w-full flex flex-wrap gap-5 px-5 justify-center items-center pt-8 pb-8 '>
-            {food_items.map((item)=>(
+            {cate.map((item)=>(
                 <Card name={item.food_name} image={item.food_image} price={item.price} id={item.id} type={item.food_type} />
             ))}
       </div>
